1c03243c736228b874463edec9ce35a47d54e821
PoChuanSu <peterpeter1010@live.com>
2025-02-25 13:17:13 +1100

    lift state up

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -6,11 +6,17 @@ const initialItems = [
 ];
 
 export default function App() {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList items={items} />
             <Stats />
         </div>
     );
@@ -19,7 +25,7 @@ export default function App() {
 function Logo() {
     return <h1>🏝️ Far Away🎒</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -34,7 +40,8 @@ function Form() {
             packed: false,
             id: Date.now(),
         };
-        console.log(newItem);
+
+        onAddItems(newItem);
 
         setDescription("");
         setQuantity(1);
@@ -63,11 +70,11 @@ function Form() {
         </form>
     );
 }
-function PackingList() {
+function PackingList({ items }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item item={item} key={item.id} />
                 ))}
             </ul>
