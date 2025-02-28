e50f91ebb3881928e173f82f829b96e9e2388bd1
PoChuanSu <peterpeter1010@live.com>
2025-02-28 19:57:36 +1100

    children props

--- steps/src/App-v1.js
+++ steps/src/App-v1.js
@@ -42,37 +42,27 @@ export default function App() {
                     </p>
 
                     <div className="buttons">
-                        <Button
-                            bgColor="#7950f2"
-                            textColor="#fff"
+                        <button
+                            style={{
+                                backgroundColor: "#7950f2",
+                                color: "#fff",
+                            }}
                             onClick={handlePrevious}
                         >
-                            <span>👈</span> Previous
-                        </Button>
-                        <Button
-                            bgColor="#7950f2"
-                            textColor="#fff"
+                            Previous
+                        </button>
+                        <button
+                            style={{
+                                backgroundColor: "#7950f2",
+                                color: "#fff",
+                            }}
                             onClick={handleNext}
                         >
-                            Next <span>👉</span>
-                        </Button>
+                            Next
+                        </button>
                     </div>
                 </div>
             )}
         </>
-    );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-    return (
-        <button
-            style={{
-                backgroundColor: bgColor,
-                color: textColor,
-            }}
-            onClick={onClick}
-        >
-            {children}
-        </button>
     );
 }
