4873b4f6b48d9cf2f351ecd06c43975f4596f2a4
PoChuanSu <peterpeter1010@live.com>
2025-03-01 12:46:31 +1100

    children props 2

--- steps/src/App.js
+++ steps/src/App.js
@@ -37,9 +37,21 @@ export default function App() {
                         <div className={step >= 2 ? "active" : ""}>2</div>
                         <div className={step >= 3 ? "active" : ""}>3</div>
                     </div>
-                    <p className="message">
-                        Step {step}: {messages[step - 1]}
-                    </p>
+
+                    <StepMessage step={step}>
+                        {messages[step - 1]}
+                        <div className="buttons">
+                            <Button
+                                bgColor="#e7e7e7"
+                                textColor="#333"
+                                onClick={() =>
+                                    alert(`Learn how to ${messages[step - 1]}`)
+                                }
+                            >
+                                Learn how
+                            </Button>
+                        </div>
+                    </StepMessage>
 
                     <div className="buttons">
                         <Button
@@ -60,6 +72,15 @@ export default function App() {
                 </div>
             )}
         </>
+    );
+}
+
+function StepMessage({ step, children }) {
+    return (
+        <p className="message">
+            <h3>Step {step}: </h3>
+            {children}
+        </p>
     );
 }
 
