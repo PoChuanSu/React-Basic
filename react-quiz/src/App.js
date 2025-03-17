18ef6c660b766affdebd5d75d5ca6efef78c1e8a
PoChuanSu <peterpeter1010@live.com>
2025-03-17 15:32:32 +1100

    load/error/ready status

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
 import Main from "./Main";
 import { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
     questions: [],
@@ -24,7 +27,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+    const numQuestions = questions.length;
 
     useEffect(function () {
         fetch("http://localhost:8000/questions")
@@ -38,8 +43,11 @@ export default function App() {
             <Header />
 
             <Main>
-                <p>1/15</p>
-                <p>Question?</p>
+                {status === "loading" && <Loader />}
+                {status === "error" && <Error />}
+                {status === "ready" && (
+                    <StartScreen numQuestions={numQuestions} />
+                )}
             </Main>
         </div>
     );
