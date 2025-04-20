d237b87ea97f54541c08099d9f0892083c0ed36d
PoChuanSu <peterpeter1010@live.com>
2025-04-20 16:49:14 +1000

    css module

---
+++ worldwise/src/pages/AppLayouts.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayouts() {
+    return (
+        <div>
+            {" "}
+            <AppNav />
+            <h1>App</h1>
+        </div>
+    );
+}
+export default AppLayouts;
--- worldwise/src/pages/Homepage.jsx
+++ worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Hoempage() {
     return (
         <div>
             <PageNav />
+            <AppNav />
             <h1>WorldWise</h1>
         </div>
     );
